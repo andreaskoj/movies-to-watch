9bfb3599463dba11e81e404f3dae31a8bdd70570
Andreas Koj <kojandreas@gmail.com>
2018-11-12 13:12:47 +0100

    fix movies search

--- js/myjs.js
+++ js/myjs.js
@@ -7,28 +7,35 @@ $(document).ready(function (){
 //5. handle errors    
 
 //key should be hidden 
-    
+        
 function searchMovies(querry) { 
-    var responseObj; 
+    var responseObj;
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://api.themoviedb.org/3/search/movie?include_adult=true&page=1&query="+ querry +"&language=en-US&api_key=3bfda1c9ae9408dc2473f92708ca7cc3",
+        "url": "https://api.themoviedb.org/3/search/movie?include_adult=true&page=1&query="+querry+"&language=en-US&api_key=3bfda1c9ae9408dc2473f92708ca7cc3",
         "method": "GET",
         "headers": {},
         "data": "{}"}
 
     $.ajax(settings).done(function (response) {       
     responseObj = response;    
-    console.log(settings.url);
-    console.log(responseObj.results[0].title);
-    console.log(responseObj.results[1].title);
-    console.log(responseObj.results[2].title); });   
+    console.log(responseObj);
+        $(".items-list-result").append("<li>"+ responseObj.results[0].title +"</li>");
+        $(".items-list-result").append("<li>"+ responseObj.results[1].title +"</li>");
+        $(".items-list-result").append("<li>"+ responseObj.results[2].title +"</li>");
+     });   
+     
+
+    //showing results here
+        
     }    
 
-    // controllerss
+    
+    // controllers
     $("#addButton").click(function (){
-            searchMovies("Batman"); 
+        var inputBoxValue = $('#searchMovies').val(); 
+        searchMovies(inputBoxValue); 
         });
    
 });
