e613febbacb2c8e1f6088e938d0fd8bd2a68ece4
Andreas Koj <kojandreas@gmail.com>
2018-11-12 17:45:08 +0100

    work on displaying search results /layout

--- js/myjs.js
+++ js/myjs.js
@@ -1,12 +1,15 @@
 $(document).ready(function (){
-//tasks
-//1. read more about sessions 
-//2. design results from searching
-//3. design dispying results 
-//4. style elements
-//5. handle errors    
-
-//key should be hidden 
+/*
+tasks
+1. read more about sessions 
+2. design results from searching
+3. design dispying results 
+4. style elements
+5. handle errors  
+6. Enter runs searching
+---------------------------------
+display movies from webstorage -> add movie -> save in ws -> update list
+*/
         
 function searchMovies(querry) { 
     var responseObj;
@@ -19,23 +22,24 @@ function searchMovies(querry) {
         "data": "{}"}
 
     $.ajax(settings).done(function (response) {       
-    responseObj = response;    
-    console.log(responseObj);
-        $(".items-list-result").append("<li>"+ responseObj.results[0].title +"</li>");
+    responseObj = response; 
+        
+        var titleItem, posterItem, yearItem, descriptionItem;
+        titleItem = responseObj.results[0].title;
+        posterItem = "http://image.tmdb.org/t/p/w92/"+responseObj.results[0].poster_path;
+        yearItem = responseObj.results[0].release_date;
+        descriptionItem = responseObj.results[0].overview;
+        
+        $(".items-list-result").append("<li class='liElement'><img class='poster' src=" +posterItem+"><div class='titleItem'>"+ titleItem +"</div><div class='yearItem'>" +yearItem+"</div><div>"+descriptionItem+"</div><img src=/assets/baseline-add-24px.svg></li>");
         $(".items-list-result").append("<li>"+ responseObj.results[1].title +"</li>");
         $(".items-list-result").append("<li>"+ responseObj.results[2].title +"</li>");
+        console.log(responseObj.results[2]);
      });   
-     
-
     //showing results here
-        
     }    
-
-    
     // controllers
-    $("#addButton").click(function (){
+     $("#addButton").on("click" , function (){
         var inputBoxValue = $('#searchMovies').val(); 
         searchMovies(inputBoxValue); 
         });
-   
 });
