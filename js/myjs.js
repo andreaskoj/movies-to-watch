2853685d102fdf2f8fb477a751e048d2e3f8558a
Andreas Koj <kojandreas@gmail.com>
2019-11-04 21:46:30 +0800

    send get request after keyup event

--- js/myjs.js
+++ js/myjs.js
@@ -5,24 +5,16 @@ tasks
 2. design results from searching
 3. design dispying results 
 4. style elements
-5. handle errors  
+5. handle errors
 6. Enter runs searching
 ---------------------------------
 display movies from webstorage -> add movie -> save in ws -> update list
 */
         
-function searchMovies(querry) { 
-    var responseObj;
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.themoviedb.org/3/search/movie?include_adult=true&page=1&query="+querry+"&language=en-US&api_key=3bfda1c9ae9408dc2473f92708ca7cc3",
-        "method": "GET",
-        "headers": {},
-        "data": "{}"}
-
-    $.ajax(settings).done(function (response) {       
-    responseObj = response; 
+function searchMovies(querry) {
+    var url ="https://api.themoviedb.org/3/search/movie?include_adult=true&page=1&query=" + querry + "&language=en-US&api_key=3bfda1c9ae9408dc2473f92708ca7cc3" 
+    
+    $.get(url, function(responseObj){
         
         var titleItem, posterItem, yearItem, descriptionItem;
         titleItem = responseObj.results[0].title;
@@ -30,16 +22,36 @@ function searchMovies(querry) {
         yearItem = responseObj.results[0].release_date;
         descriptionItem = responseObj.results[0].overview;
         
-        $(".items-list-result").append("<li class='liElement'><img class='poster' src=" +posterItem+"><div class='titleItem'>"+ titleItem +"</div><div class='yearItem'>" +yearItem+"</div><div>"+descriptionItem+"</div><img src=/assets/baseline-add-24px.svg></li>");
+        $(".items-list-result").append("<li class='liElement'><img class='poster' src=" +posterItem+"><div class='titleItem'>"+ titleItem +"</div><div class='yearItem'>" + yearItem +"</div><div>"+descriptionItem+"</div><img src=/assets/baseline-add-24px.svg></li>");
+        
         $(".items-list-result").append("<li>"+ responseObj.results[1].title +"</li>");
         $(".items-list-result").append("<li>"+ responseObj.results[2].title +"</li>");
-        console.log(responseObj.results[2]);
-     });   
-    //showing results here
-    }    
-    // controllers
-     $("#addButton").on("click" , function (){
-        var inputBoxValue = $('#searchMovies').val(); 
-        searchMovies(inputBoxValue); 
+        console.log(responseObj.results[2]);    
+    })
+}    
+        
+    // event handlers 
+    $("#addButton").on("click" , function (){
+//            searchMovies($('#searchMovies').val()); 
+            searchMovies($('#searchMoviesInput').val()); 
         });
+    
+    var typingInterval = 800;
+    var timeoutRef;
+    
+    $("#searchMoviesInput").on('keyup', function(){
+        console.log("keyup")
+        clearTimeout(timeoutRef);
+        
+            if($("#searchMoviesInput").val()) {
+                console.log("time ref")
+                timeoutRef = setTimeout(finishedTyping, typingInterval);
+            }
+    })
+    
+    function finishedTyping() {
+            console.log($('#searchMoviesInput').val());
+            searchMovies($('#searchMoviesInput').val()); 
+    }
+    
 });
